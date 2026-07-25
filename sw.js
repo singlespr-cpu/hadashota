cd10c141cfe88e4e7c78ca1f052a134d64447994
singlespr-cpu <singles.pr@gmail.com>
2026-07-26 01:55:22 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const HADASHOTA_SW_VERSION = "69.0.0";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  const targetUrl = event.notification?.data?.url || "/";
+  event.waitUntil((async () => {
+    try {
+      if (self.clients.openWindow) await self.clients.openWindow(targetUrl);
+    } catch {
+      if (self.clients.openWindow) await self.clients.openWindow("/");
+    }
+  })());
+});
